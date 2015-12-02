4eeed89c62b1ede86004d47cb73915521e22cae6
Curtis Maloney <curtis@tinbrain.net>
2015-12-02 23:26:25 +1100

    Fixed stylesheet editor js

--- kopytka/static/kopytka/js/editor_stylesheet.js
+++ kopytka/static/kopytka/js/editor_stylesheet.js
@@ -1,6 +1,6 @@
 django.jQuery(document).ready(function () {
     try {
-        editor = make_editor('id_content', {mode: 'text/x-scss'});
+        editor = make_editor('id_source', {mode: 'text/x-scss'});
         add_button({
             title: 'Reindent',
             action: reindent,
