4b2cd1442a23326c4df782460439713b939e31ca
Curtis Maloney <curtis@tinbrain.net>
2015-12-02 17:04:34 +1100

    Simplify page view to a function

--- kopytka/urls.py
+++ kopytka/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^css/(?P<name>[-\w]+)\.css$', views.style_sheet, name='style-sheet'),
-    url(r'(?P<path>.*)$', views.PageView.as_view(), name='page'),
+    url(r'(?P<path>.*)$', views.page, name='page'),
 ]
--- kopytka/views.py
+++ kopytka/views.py
@@ -1,39 +1,28 @@
 from django.http import Http404, HttpResponse
-from django.shortcuts import get_object_or_404
-from django.views import generic
+from django.shortcuts import get_object_or_404, render
 
 from . import models
 
 
-class PageView(generic.DetailView):
-    model = models.Page
+def page(request, path):
 
-    def get_queryset(self):
-        return models.Page.objects.published().select_related('template')
+    paths = list(filter(None, path.split('/'))) or ['', ]
 
-    def get_object(self, queryset=None):
-        if queryset is None:
-            queryset = self.get_queryset()
+    paths.reverse()
 
-        paths = list(filter(None, self.kwargs.get('path', '/').split('/'))) or ['', ]
+    query = {}
+    pfx = 'path'
+    for step in paths:
+        query[pfx] = step
+        pfx = 'parent__%s' % pfx
+    query[pfx.replace('path', 'isnull')] = True
 
-        paths.reverse()
+    try:
+        page = models.Page.objects.published().get(**query)
+    except models.Page.DoesNotExist:
+        raise Http404
 
-        query = {}
-        pfx = 'path'
-        for step in paths:
-            query[pfx] = step
-            pfx = 'parent__%s' % pfx
-        query[pfx.replace('path', 'isnull')] = True
-
-        try:
-            page = queryset.get(**query)
-        except models.Page.DoesNotExist:
-            raise Http404
-        return page
-
-    def get_template_names(self):
-        return self.object.get_template(),
+    return render(request, page.get_template(), {'page': page})
 
 
 def style_sheet(request, name):
