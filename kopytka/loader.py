8755bcf103fbb2d706aa33013622f484b7653de3
Curtis Maloney <curtis@tinbrain.net>
2015-12-07 15:06:57 +1100

    Support recursive template loading

--- kopytka/loader.py
+++ kopytka/loader.py
@@ -1,10 +1,17 @@
-from django.template import TemplateDoesNotExist
+from django.template import Origin, TemplateDoesNotExist
 from django.template.loaders.base import Loader
 
 from .models import Template
 
 
 class DBLoader(Loader):
+
+    def get_contents(self, origin):
+        return Template.objects.get(pk=origin.name).content
+
+    def get_template_sources(self, template_name):
+        for tmpl in Template.objects.filter(name=template_name):
+            yield Origin(name=tmpl.pk, template_name=template_name, loader=self)
 
     def load_template_source(self, template_name, template_dirs=None):
         try:
