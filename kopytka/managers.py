d5c65f6ac2cdae3310f41efb9ab0a6d5cae63357
Curtis Maloney <curtis@tinbrain.net>
2016-01-16 13:42:31 +1100

    Add fragment_keys method to PageQuerySet

--- kopytka/managers.py
+++ kopytka/managers.py
@@ -1,7 +1,11 @@
 from django.db import models
+from .transforms import SKeys
 
 
 class PageQuerySet(models.QuerySet):
 
     def published(self):
         return self.filter(is_published=True)
+
+    def fragment_keys(self):
+        return self.annotate(keys=SKeys('fragments')).values_list('keys', flat=True)
